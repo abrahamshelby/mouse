1b8c4126e9703c66c638f07d4455e85fce39ce27
Sweetcheese <shihabraham@gmail.com>
2024-03-07 15:41:02 +0800

    lesson7

---
+++ project7.py
@@ -0,0 +1,23 @@
+# while 迴圈
+# 1+2+3+...+10
+n=1
+sum=0 #紀錄累加的結果
+while n<=10:
+    sum=sum+n
+    n+=1
+print(sum)
+
+# for 迴圈
+for x in [3,4,6]: 
+    print(x)
+for x in "apple":
+    print(x)
+# 1+2+3...+10
+sum=0
+for n in range(1,11):
+    sum=sum+n
+print(sum)
+
+
+
+
